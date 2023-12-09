42abe9c993ab53739787be61b51a59d6ac556c0d
Daniel Josias <daniieljosiaslima@gmail.com>
2023-12-09 17:54:38 -0300

    feat: tela de entrar no sistema

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -6,8 +6,8 @@ import {
   Input,
   Button,
   Heading,
-  HStack,
   Flex,
+  Box,
 } from '@chakra-ui/react'
 
 export const Signin = () =>{
@@ -22,17 +22,18 @@ export const Signin = () =>{
   const isEmailError = password === ''
   
   return(
-    <Flex flexDirection='column' alignItems='center' >
-      <Heading as='h1'>Entrar</Heading>
-      <FormControl display='flex' flexDirection='column' justifyContent='center' gap='15' isInvalid={{isNameError, isEmailError}} bg='#F3CBCB' borderRadius='0px 10px 0px 10px' p='25' h='200px' w='300px'>
-        {name === '' ? <FormErrorMessage fontWeight='bold'>Nome é obrigatório.</FormErrorMessage> : <FormLabel fontWeight='bold'>Nome</FormLabel>}
-        <Input type='text' value={name} onChange={handleNameChange} border='none' p='4' bg='#000000' color='#FFFFFF' borderRadius='6px' w='100%' fontWeight='bold' fontSize='large'/>
+    <Flex alignItems='center' justifyContent='center' textAlign='center'>
+      <FormControl display='flex' flexDirection='column' justifyContent='center'isInvalid={{isNameError, isEmailError}} bg='#F3CBCB' borderRadius='0px 10px 0px 10px' p='25' h='350px' w='300px'>
+        <Heading as='h1' >Entrar</Heading>
         
-        {password === '' ? <FormErrorMessage fontWeight='bold'>Senha é obrigatória.</FormErrorMessage> : <FormLabel fontWeight='bold'>Senha</FormLabel>}
-        <Input type='password' value={password} onChange={handlePasswordChange} border='none' p='4' bg='#000000' color='#FFFFFF' borderRadius='6px' w='100%' fontWeight='bold' fontSize='large'/>
+        {name === '' ? <FormErrorMessage fontWeight='bold'>Nome é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' >Nome</FormErrorMessage>}
+        <Input type='text' value={name} onChange={handleNameChange} border='none' bg='#000000' color='#FFFFFF' borderRadius='6px' fontWeight='bold' fontSize='large'/>
         
-        <Button type='submit' bg='#FFFFFF' w='100%' border='none' borderRadius='10px' p='6' fontWeight='bold' mt='5' cursor='pointer' fontSize='large' _hover={'black'}>Entrar</Button>
+        {password === '' ? <FormErrorMessage fontWeight='bold'>Senha é obrigatória.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' >Senha</FormErrorMessage>}
+        <Input type='password' value={password} onChange={handlePasswordChange} border='none' bg='#000000' color='#FFFFFF' borderRadius='6px' fontWeight='bold' fontSize='large'/>
+        
+        <Button type='submit' bg='#FFFFFF' w='100%' border='none' borderRadius='10px' fontWeight='bold' mt='15' cursor='pointer' fontSize='large' _hover={'black'}>Entrar</Button>
       </FormControl>
-    </Flex>
+  </Flex>
   )
 }
