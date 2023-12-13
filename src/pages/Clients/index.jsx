b0867787960cda7fe41f34b5a1c69859103d8b2f
Daniel Josias <daniieljosiaslima@gmail.com>
2023-12-13 09:33:25 -0300

    feat: formulário de criação de clientes

--- src/pages/Clients/index.jsx
+++ src/pages/Clients/index.jsx
@@ -2,44 +2,57 @@ import { Header } from '../../components/Header'
 import { useState } from 'react'
 import {
   FormControl,
-  FormLabel,
   FormErrorMessage,
   Input,
   Button,
   Heading,
   Flex,
-  Box,
   Icon,
+  Box,
+  HStack,
+  VStack,
 } from '@chakra-ui/react'
+import { IoMdPeople } from "react-icons/io";
 
 export const Clients = () => {
   const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
+  const [address, setAddress] = useState('')
+  const [cellphone, setCellphone] = useState('')
 
   const handleNameChange = (e) => setName(e.target.value)
-
-  const handlePasswordChange = (e) => setPassword(e.target.value)
+  const handleAddressChange = (e) => setAddress(e.target.value)
+  const handleCellphoneChange = (e) => setCellphone(e.target.value)
   
   const isNameError = name === ''
-  const isEmailError = password === ''
+  const isAddressError = address === ''
+  const isCelphoneError = cellphone === ''
 
   return(
     <>
       <Header/>
-      <Flex alignItems='center' justifyContent='center' textAlign='center'>
-        <FormControl display='flex' flexDirection='column' justifyContent='center'isInvalid={{isNameError, isEmailError}} bg='#F3CBCB' borderRadius='0px 10px 0px 10px' p='25' h='350px' w='300px'>
-          <Heading as='h3' >Criação de Clientes</Heading>
-          <Icon as={MdSettings} />
+      <VStack>
+        <HStack spacing={10} h='30px' textAlign='center'>
+          <Box h='0px'>
+            <Heading as='h3'>Criação de Clientes</Heading>
+          </Box>
+          <Box h='0px'>
+            <Icon as={IoMdPeople} fontSize='20px'/>
+          </Box>
+        </HStack>
+
+        <FormControl isInvalid={{isNameError, isAddressError, isCelphoneError}} display='flex' gap='10px' flexDirection='column' justifyContent='center' bg='#F3CBCB' borderRadius='0px 10px 0px 10px' p='25' h='450px' w='300px'>
+          {name === '' ? <FormErrorMessage fontWeight='bold' h='30px'>Nome é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' h='30px'>Nome</FormErrorMessage>}
+          <Input h='30px' type='text' value={name} onChange={handleNameChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite o nome do cliente'/>
           
-          {name === '' ? <FormErrorMessage fontWeight='bold'>Nome é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' >Nome</FormErrorMessage>}
-          <Input type='text' value={name} onChange={handleNameChange} border='none' bg='#000000' color='#FFFFFF' borderRadius='6px' fontWeight='bold' fontSize='large'/>
+          {address === '' ? <FormErrorMessage fontWeight='bold' h='30px'>Endereço é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' h='30px'>Endereço</FormErrorMessage>}
+          <Input h='30px' type='text' value={address} onChange={handleAddressChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite o endereço do cliente'/>
+
+          {cellphone === '' ? <FormErrorMessage fontWeight='bold' h='30px'>Celular é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' h='30px'>Celular</FormErrorMessage>}
+          <Input h='30px' type='text' value={cellphone} onChange={handleCellphoneChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite o celular do cliente'/>
           
-          {password === '' ? <FormErrorMessage fontWeight='bold'>Senha é obrigatória.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' >Senha</FormErrorMessage>}
-          <Input type='password' value={password} onChange={handlePasswordChange} border='none' bg='#000000' color='#FFFFFF' borderRadius='6px' fontWeight='bold' fontSize='large'/>
-          
-          <Button type='submit' bg='#FFFFFF' w='100%' border='none' borderRadius='10px' fontWeight='bold' mt='15' cursor='pointer' fontSize='large' _hover={'black'}>Entrar</Button>
+          <Button h='30px' type='submit' bg='#000000' color='white' border='none' borderRadius='10px' fontWeight='bold' mt='15' cursor='pointer' fontSize='large' _hover={'black'}>Entrar</Button>
         </FormControl>
-      </Flex>
+      </VStack>
     </>
   )
 }
