33c5f0dc89139ff43ba9d5c63fa5c064a5b424a7
Daniel Josias <daniieljosiaslima@gmail.com>
2023-12-17 11:04:41 -0300

    feat: ...

--- src/components/ClientsCard/index.jsx
+++ src/components/ClientsCard/index.jsx
@@ -3,17 +3,17 @@ import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
 
 export const ClientsCard = () => {
     return(
-        <Flex bg='white' h='200px' borderRadius='0px 10px 0px 10px' p='10' >
+        <Flex bg='white' h='200px' p='25' gap='20px' borderRadius='0px 10px 0px 10px'>
             <Flex flexDirection='column' h='0' gap='10' p='10'>
                 <Heading as='h3'>Nome: xxxxxxxxxxxxxxxxxxxxx</Heading>
                 <Heading as='h3'>Endereço: xxxxxxxxxxxxxxxxxxxxx</Heading>
                 <Heading as='h3'>Celular: xxxxxxxxxxxxxxxxxxxxx</Heading>
                 <Heading as='h3'>Observações: xxxxxxxxxxxxxxxxxxxxx</Heading>
             </Flex>
-            {/* <FormControl display='flex' position='absolute' left='180px' top='20px' justifyContent='center' textAlign='center' alignItems='center' bg='#D9D9D9' w='100px' h='100px' borderRadius='0px 10px 0px 10px'>
+            <FormControl display='flex' top='20px' justifyContent='center' textAlign='center' alignItems='center' bg='#D9D9D9' w='100px' h='100px' borderRadius='0px 10px 0px 10px'>
                 <Input for='file' type='file'/>
                 <FormLabel id='file' fontWeight='bold' h='0'>Foto</FormLabel>
-            </FormControl> */}
+            </FormControl>
             <Flex h='0'>
                 <IconButton aria-label='editar' icon={<DeleteIcon />} color='blue' border='none' bg='transparent'/>
                 <IconButton aria-label='deletar' icon={<EditIcon />}color='red' border='none' bg='transparent'/>
--- src/pages/Clients/index.jsx
+++ src/pages/Clients/index.jsx
@@ -33,53 +33,57 @@ export const Clients = () => {
   const isCelphoneError = cellphone === ''
 
   return(
-    <Flex flexDirection='column'>
+    <>
       <Header isOpen={isOpen} setIsOpen={setIsOpen}/>
-      <Box m='20px'>
-        <HStack spacing={10} h='30px' textAlign='center' alignItems='center' justifyContent='center'>
-          <Box h='0px'>
-            <Heading as='h3' h='0px'>Criação de Clientes</Heading>
-          </Box>
-          <Box h='0px'>
-            <Icon as={IoMdPeople} fontSize='20px'/>
-          </Box>
-        </HStack>
+      <Flex flexDirection='column' gap='20px' p='20px'>
+        <Box>
+          <HStack spacing={10} h='30px' textAlign='center' alignItems='center' justifyContent='center'>
+            <Box h='0px'>
+              <Heading as='h3' h='0px'>Criação de Clientes</Heading>
+            </Box>
+            <Box h='0px'>
+              <Icon as={IoMdPeople} fontSize='20px'/>
+            </Box>
+          </HStack>
+        </Box>
 
-        <FormControl isInvalid={{isNameError, isAddressError, isCelphoneError}} display='flex' gap='10px' flexDirection='column' justifyContent='center' bg='#F3CBCB' borderRadius='0px 10px 0px 10px' p='25' h='450px'>
-          {name === '' ? <FormErrorMessage fontWeight='bold' h='30px'>Nome é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' h='30px'>Nome</FormErrorMessage>}
-          <Input h='30px' type='text' value={name} onChange={handleNameChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite o nome do cliente'/>
-          
-          {address === '' ? <FormErrorMessage fontWeight='bold' h='30px'>Endereço é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' h='30px'>Endereço</FormErrorMessage>}
-          <Input h='30px' type='text' value={address} onChange={handleAddressChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite o endereço do cliente'/>
+        <Box>
+          <FormControl isInvalid={{isNameError, isAddressError, isCelphoneError}} display='flex' gap='10px' flexDirection='column' justifyContent='center' bg='#F3CBCB' borderRadius='0px 10px 0px 10px' p='25' h='450px'>
+            {name === '' ? <FormErrorMessage fontWeight='bold' h='30px'>Nome é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' h='30px'>Nome</FormErrorMessage>}
+            <Input h='30px' type='text' value={name} onChange={handleNameChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite o nome do cliente'/>
+            
+            {address === '' ? <FormErrorMessage fontWeight='bold' h='30px'>Endereço é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' h='30px'>Endereço</FormErrorMessage>}
+            <Input h='30px' type='text' value={address} onChange={handleAddressChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite o endereço do cliente'/>
 
-          {cellphone === '' ? <FormErrorMessage fontWeight='bold' h='30px'>Celular é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' h='30px'>Celular</FormErrorMessage>}
-          <Input h='30px' type='text' value={cellphone} onChange={handleCellphoneChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite o celular do cliente'/>
-          
-          <FormLabel fontWeight='bold' h='25px'>Observações</FormLabel>
-          <Input h='30px' type='text' value={observation} onChange={handleObservationChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite algo sobre o cliente'/>
-         
-          {!isOpen ?  <Button h='30px' type='submit' bg='#000000' color='white' border='none' borderRadius='10px' fontWeight='bold' mt='15' cursor='pointer' fontSize='large' _hover={{background: 'white', color: 'black', transition: 'ease 1s'}}>Criar</Button>
-          :
-          <Button display='none' h='30px' type='submit' bg='#000000' color='white' border='none' borderRadius='10px' fontWeight='bold' mt='15' cursor='pointer' fontSize='large' _hover={{background: 'white', color: 'black', transition: 'ease 1s'}}>Criar</Button>
-          }
-        </FormControl>
-      </Box>
+            {cellphone === '' ? <FormErrorMessage fontWeight='bold' h='30px'>Celular é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' h='30px'>Celular</FormErrorMessage>}
+            <Input h='30px' type='text' value={cellphone} onChange={handleCellphoneChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite o celular do cliente'/>
+            
+            <FormLabel fontWeight='bold' h='25px'>Observações</FormLabel>
+            <Input h='30px' type='text' value={observation} onChange={handleObservationChange} border='none' bg='#FFFFFF' color='#000000' borderRadius='6px' fontWeight='bold' fontSize='large' placeholder='Digite algo sobre o cliente'/>
+            
+            {!isOpen ?  <Button h='30px' type='submit' bg='#000000' color='white' border='none' borderRadius='10px' fontWeight='bold' mt='15' cursor='pointer' fontSize='large' _hover={{background: 'white', color: 'black', transition: 'ease 1s'}}>Criar</Button>
+            :
+            <Button display='none' h='30px' type='submit' bg='#000000' color='white' border='none' borderRadius='10px' fontWeight='bold' mt='15' cursor='pointer' fontSize='large' _hover={{background: 'white', color: 'black', transition: 'ease 1s'}}>Criar</Button>
+            }
+          </FormControl>
+        </Box>
+        
+        <Flex flexDirection='column' gap='20px'>
+          <HStack spacing={10} h='30px' textAlign='center' alignItems='center' justifyContent='center'>
+            <Box h='0px'>
+              <Heading as='h3'>Listagem de Clientes</Heading>
+            </Box>
+            <Box h='0px'>
+              <Icon as={IoMdPeople} fontSize='20px'/>
+            </Box>
+          </HStack>
 
-      <Flex flexDirection='column' /* alignItems='center' gap='10' p='30px' */>
-        <HStack spacing={10} h='30px' textAlign='center' alignItems='center'>
-          <Box h='0px'>
-            <Heading as='h3'>Listagem de Clientes</Heading>
-          </Box>
-          <Box h='0px'>
-            <Icon as={IoMdPeople} fontSize='20px'/>
-          </Box>
-        </HStack>
-
-        <Flex bg='#F3CBCB' flexDirection='column' borderRadius='0px 10px 0px 10px' p='25'>
-          <ClientsCard/>
-        </Flex>    
-
+          <Flex bg='#F3CBCB' flexDirection='column' p='25' gap='20px' borderRadius='0px 10px 0px 10px' >
+            <ClientsCard/>
+            <ClientsCard/>
+          </Flex>    
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   )
 }
