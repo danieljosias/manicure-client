7f9fe62d60ae1b813dc08ea9362dc4546e13eaa6
Daniel Josias <daniieljosiaslima@gmail.com>
2023-12-17 11:25:04 -0300

    feat: ...

--- src/components/ClientsCard/index.jsx
+++ src/components/ClientsCard/index.jsx
@@ -1,23 +1,26 @@
-import { Card, Text, IconButton, FormControl, Input, Box, Flex, FormLabel, VStack, Heading} from '@chakra-ui/react'
+import { Card, Text, IconButton, FormControl, Input, Box, Flex, FormLabel, VStack, Heading, HStack} from '@chakra-ui/react'
 import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
 
 export const ClientsCard = () => {
     return(
-        <Flex justifyContent='space-around' bg='white' p='25' borderRadius='0px 10px 0px 10px' >
-            <Flex flexDirection='column' h='0' gap='10' p='10'>
-                <Heading as='h3'>Nome: xxxxxxxxxxxxxxxxxxxxx</Heading>
-                <Heading as='h3'>Endereço: xxxxxxxxxxxxxxxxxxxxx</Heading>
-                <Heading as='h3'>Celular: xxxxxxxxxxxxxxxxxxxxx</Heading>
-                <Heading as='h3'>Observações: xxxxxxxxxxxxxxxxxxxxx</Heading>
-            </Flex>
+        <Flex bg='white' justifyContent='space-between' p='25' borderRadius='0px 10px 0px 10px' h='200px'>
+            <Box>
+                <Heading as='h3' h='40'>Nome: xxxxxxxxxxxxxxxxxxxxx</Heading>
+                <Heading as='h3' h='40'>Endereço: xxxxxxxxxxxxxxxxxxxxx</Heading>
+                <Heading as='h3' h='40'>Celular: xxxxxxxxxxxxxxxxxxxxx</Heading>
+                <Heading as='h3' h='40'>Observações: xxxxxxxxxxxxxxxxxxxxx</Heading>
+            </Box>
+            
+            <Box>
+                <HStack h='0'>
+                    <IconButton aria-label='editar' icon={<DeleteIcon />} color='blue' border='none' bg='transparent' h='0' fontSize='20px'/>
+                    <IconButton aria-label='deletar' icon={<EditIcon />}color='red' border='none' bg='transparent' h='0' fontSize='20px'/>
+                </HStack>
+            </Box>
             {/* <FormControl display='none' justifyContent='center' textAlign='center' alignItems='center' bg='#D9D9D9' w='100px' h='100px' borderRadius='0px 10px 0px 10px'>
                 <Input for='file' type='file'/>
                 <FormLabel id='file' fontWeight='bold' h='0'>Foto</FormLabel>
             </FormControl> */}
-            <Flex h='0'>
-                <IconButton aria-label='editar' icon={<DeleteIcon />} color='blue' border='none' bg='transparent' h='0'/>
-                <IconButton aria-label='deletar' icon={<EditIcon />}color='red' border='none' bg='transparent' h='0'/>
-            </Flex>
         </Flex>
     )   
 }
