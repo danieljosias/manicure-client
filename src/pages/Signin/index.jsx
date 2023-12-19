bff533c5376961fa496227f8439931784baa925c
Daniel Josias <daniieljosiaslima@gmail.com>
2023-12-19 09:37:03 -0300

    fix: tela de login corrigida

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -15,24 +15,24 @@ export const Signin = () =>{
   const [password, setPassword] = useState('')
 
   const handleNameChange = (e) => setName(e.target.value)
-
   const handlePasswordChange = (e) => setPassword(e.target.value)
   
   const isNameError = name === ''
   const isEmailError = password === ''
   
   return(
-    <Flex alignItems='center' justifyContent='center' textAlign='center'>
-      <FormControl display='flex' flexDirection='column' justifyContent='center'isInvalid={{isNameError, isEmailError}} bg='#F3CBCB' borderRadius='0px 10px 0px 10px' p='25' h='350px' w='300px'>
-        <Heading as='h1' >Entrar</Heading>
+    <Flex flexDirection='column' alignItems='center' justifyContent='center' textAlign='center' h='100vh' gap='5'>
+      <Heading as='h1' >Entrar</Heading>
+     
+      <FormControl display='flex' flexDirection='column' justifyContent='center'isInvalid={{isNameError, isEmailError}} bg='#F3CBCB' borderRadius='0px 10px 0px 10px' p='25' h='300px' w='300px' gap='20px'>
         
-        {name === '' ? <FormErrorMessage fontWeight='bold'>Nome é obrigatório.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' >Nome</FormErrorMessage>}
-        <Input type='text' value={name} onChange={handleNameChange} border='none' bg='#000000' color='#FFFFFF' borderRadius='6px' fontWeight='bold' fontSize='large'/>
+        {name === '' ? <FormErrorMessage h='0px' fontWeight='bold'>Nome é obrigatório.</FormErrorMessage> : <FormErrorMessage h='0px' fontWeight='bold' >Nome</FormErrorMessage>}
+        <Input h='40px' type='text' value={name} onChange={handleNameChange} border='none' bg='#000000' color='#FFFFFF' borderRadius='6px' fontWeight='bold' fontSize='large'/>
         
-        {password === '' ? <FormErrorMessage fontWeight='bold'>Senha é obrigatória.</FormErrorMessage> : <FormErrorMessage fontWeight='bold' >Senha</FormErrorMessage>}
-        <Input type='password' value={password} onChange={handlePasswordChange} border='none' bg='#000000' color='#FFFFFF' borderRadius='6px' fontWeight='bold' fontSize='large'/>
+        {password === '' ? <FormErrorMessage h='0px' fontWeight='bold'>Senha é obrigatória.</FormErrorMessage> : <FormErrorMessage h='0px' fontWeight='bold' >Senha</FormErrorMessage>}
+        <Input h='40px' type='password' value={password} onChange={handlePasswordChange} border='none' bg='#000000' color='#FFFFFF' borderRadius='6px' fontWeight='bold' fontSize='large'/>
         
-        <Button type='submit' bg='#FFFFFF' w='100%' border='none' borderRadius='10px' fontWeight='bold' mt='15' cursor='pointer' fontSize='large' _hover={'black'}>Entrar</Button>
+        <Button h='40px' type='submit' bg='#FFFFFF' w='100%' border='none' borderRadius='10px' fontWeight='bold' mt='15' cursor='pointer' fontSize='large' _hover={{'background':'black', 'color':'white'}} transition='ease 1s'>Entrar</Button>
       </FormControl>
   </Flex>
   )
