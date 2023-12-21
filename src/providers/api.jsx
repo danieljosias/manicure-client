e2576308715a2f709b901f2f242ceea16bb1483b
Daniel Josias <daniieljosiaslima@gmail.com>
2023-12-20 21:09:19 -0300

    feat: providers e services

--- src/providers/api.jsx
+++ src/providers/api.jsx
@@ -50,7 +50,7 @@ export const ApiProvider = ({children}) => {
         }
     }
 
-    async function deleteComments(user_id){
+    async function deleteClients(user_id){
         try {
             const res = await api.delete(`/comments/${user_id}/`)
             return res
@@ -59,6 +59,79 @@ export const ApiProvider = ({children}) => {
         }
     }
 
+    async function createSchedules(data){
+        try {
+            const res = await api.post('/schedules/', data)
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function listSchedules(){
+        try {
+            const res = await api.get('/schedules/')
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function updateSchedules(user_id, data){
+        try {
+            const res = await api.patch(`/schedules/${user_id}/`, data)
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function deleteSchedules(user_id){
+        try {
+            const res = await api.delete(`/schedules/${user_id}/`)
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function createFinances(data){
+        try {
+            const res = await api.post('/finances/', data)
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function listFinances(){
+        try {
+            const res = await api.get('/finances/')
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function updateFinances(user_id, data){
+        try {
+            const res = await api.patch(`/finances/${user_id}/`, data)
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function deleteFinances(user_id){
+        try {
+            const res = await api.delete(`/finances/${user_id}/`)
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+
     return(
         <ApiContext.Provider 
             value={{createAdmin,
@@ -66,7 +139,15 @@ export const ApiProvider = ({children}) => {
             createClients,
             listClients,
             updateCLients,
-            deleteComments, 
+            deleteClients,
+            createSchedules,
+            listSchedules,
+            updateSchedules,
+            deleteSchedules, 
+            createFinances,
+            listFinances,
+            updateFinances,
+            deleteFinances, 
         }}
         >
             {children}
